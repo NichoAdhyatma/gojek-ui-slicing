91c139ad3927799cee3d0b5e604c0846caf82b8a
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-02-26 22:33:26 +0700

    init-theme-asset-model

---
+++ lib/models/gojek_icon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GojekIcon {
+  final String title;
+  final Color? color;
+  final String icon;
+
+  GojekIcon({required this.title,this.color, required this.icon});
+}
---
+++ lib/models/news.dart
@@ -0,0 +1,7 @@
+class News {
+  final String title;
+  final String desc;
+  final String image;
+
+  News(this.title, this.desc, this.image);
+}
